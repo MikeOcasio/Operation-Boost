d7b69f5ad40e8625c96822bf44a8b714a2ac7a60
xxvxxr <xvrbradley@gmail.com>
2024-04-09 18:06:19 -0600

    Add axios library and update login form styling

--- frontend_boost/boost/src/app/login/page.jsx
+++ frontend_boost/boost/src/app/login/page.jsx
@@ -1,37 +1,38 @@
+'use client'
+
 import axios from 'axios';
 // import React from 'react';
 
 export default function Login() {
-  // const handleSubmit = async (event) => {
-    // event.preventDefault();
-// 
-    // const email = event.target.elements.email.value;
-    // const password = event.target.elements.password.value;
-// 
-    // try {
-      // const response = await axios.post('/api/login', {
-        // email: email,
-        // password: password,
-      // });
-// 
-      // Handle the response from the server
-      // console.log(response.data);
-    // } catch (error) {
-      // Handle the error
-      // console.error(error);
-    // }
-  // };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.password.value;
+
+    try {
+      const response = await axios.post('/api/login', {
+        email: email,
+        password: password,
+      });
+
+     
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-200">
-      <form /*onSubmit={handleSubmit}*/ className="p-6 w-64 bg-white rounded shadow">
-        <h2 className="text-2xl font-bold mb-5 text-gray-900">Login</h2>
+      <form /*onSubmit={handleSubmit}*/ className="w-64 p-6 bg-white rounded shadow">
+        <h2 className="mb-5 text-2xl font-bold text-gray-900">Login</h2>
 
         <input
           name="email"
           type="email"
           placeholder="Email"
-          className="block w-full p-2 border border-gray-300 rounded mb-4"
+          className="block w-full p-2 mb-4 border border-gray-300 rounded"
           required
         />
 
@@ -39,11 +40,11 @@ export default function Login() {
           name="password"
           type="password"
           placeholder="Password"
-          className="block w-full p-2 border border-gray-300 rounded mb-4"
+          className="block w-full p-2 mb-4 border border-gray-300 rounded"
           required
         />
 
-        <button type="submit" className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600">
+        <button type="submit" className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
           Login
         </button>
       </form>
